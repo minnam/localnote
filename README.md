24c8923a90dc3bb949e7f1734ad1a3a726efdf09
Min Nam <hi@minnam.io>
2021-04-15 15:45:37 -0700

    chore: upadated console

--- README.md
+++ README.md
@@ -3,10 +3,15 @@
 A simple and !!unsecure!! web-socket based wysiwyg editor for your dev workflow. Handy when you need to transfer text/files between machines on the same network
 
 # Getting Started
-Run it on the default port, 8888
+Run localnote on the default port, 8888
 
 `npx localnote`
 
-Run it on port 3000
+Run localnote on port 3000
 
 `npx localnote -p 3000`
+
+Run localnote with bigger buffer (default to 1e8)
+
+`npx localnote -p 3000 -b 1e10`
+
--- index.js
+++ index.js
@@ -4,17 +4,18 @@ const app = express()
 const http = require('http')
 const server = http.createServer(app)
 const args = require('minimist')(process.argv.slice(2));
-
+var os = require('os');
 
 const DEFAULT_ARGS = {
   p: 8888,
   port: 8888,
+  b: 1e8, // 100mb
   buffer: 1e8, // 100mb
   ...args
 }
 
 const io = require('socket.io')(server, {
-  maxHttpBufferSize: 1e8
+  maxHttpBufferSize: DEFAULT_ARGS.b
 })
 
 app.get('/', (req, res) => {
@@ -24,11 +25,27 @@ app.get('/', (req, res) => {
 io.on('connection', (socket) => {
   const address = socket.handshake.address;
 
+  console.log(`[${new Date().toLocaleString()}] ${address} has connected.`)
+
   socket.on('msg', (msg) => {
     io.emit('msg', { ip: address, content: msg, time: new Date().toLocaleString() });
   })
 })
 
 server.listen(DEFAULT_ARGS.p, () => {
-  console.log(`localnote running on *:${DEFAULT_ARGS.p}`)
+  const networkInterfaces = os.networkInterfaces();
+
+  console.log('')
+  console.log(`✌ localnote running at:`)
+  console.log('')
+  for (const key in networkInterfaces) {
+    networkInterfaces[key].map(a => {
+      if (a.address.match(/^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/)) {
+        console.log(`   http://${a.address}:${DEFAULT_ARGS.p}`)
+      }
+    })
+  }
+  console.log('')
+  console.log('――――――――――――――――――――――――――――')
+  console.log('')
 })
